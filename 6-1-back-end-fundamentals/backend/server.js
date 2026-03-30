c2a69b67e9d501655b214a0dee3accaab448bf3e
Fatimah Al Jawad <s202248340@kfupm.edu.sa>
2026-03-30 13:29:25 +0300

    t2&3

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -1,5 +1,14 @@
 // TODO 1: Import Express
-import express from "express";
+import express from 'express';
+
+
+// TODO 2: Create the Express app and store it in a variable named app
+const app = express();
+
+
+// TODO 3: Allow React to access the server
+import cors from 'cors'
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 
 
