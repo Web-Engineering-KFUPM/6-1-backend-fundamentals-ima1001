860ea44cf19a4b3148957e8f5f8250f28e705697
Fatimah Al Jawad <s202248340@kfupm.edu.sa>
2026-03-30 13:21:55 +0300

    t1

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -1,25 +1,5 @@
 // TODO 1: Import Express
+import express from "express";
 
 
 
-// TODO 2: Create the Express app and store it in a variable named app
-
-
-
-// TODO 3: Allow React to access the server
-
-
-
-// TODO 5: Create the home route "/"
-
-
-
-// TODO 6: Create the "/about" route
-
-
-
-// TODO 7: Create the "/student" route
-
-
-
-// TODO 4: Start the server on port 3000
